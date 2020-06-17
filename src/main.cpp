ae5bc7c27b418a7cd92570235a9b7282d6e9a1f7
varununayak <varunnayak3101@gmail.com>
2020-06-17 16:00:57 -0700

    float input isntead of hard coded values

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,22 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     return std::move(contents);
 }
 
+static float getFloatInput(const std::string variableName) {
+    while (true) {
+        std::cout << variableName << ": ";
+        std::string inp;
+        std::cin >> inp;
+        try {
+            std::size_t pos;
+            float val = std::stof(inp, &pos);
+            if (pos != inp.size()) std::cout << "Ignoring trailing characters...\n";
+            return val;
+        } catch (...) {
+            std::cout << "Please enter float value only.\n";
+        }
+    }
+}
+
 int main(int argc, const char **argv)
 {    
     std::string osm_data_file = "";
@@ -52,15 +68,16 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
-
+    std::cout << "Enter Start and End Positions in Metres:\n";
+    const float start_x = getFloatInput("start_x");
+    const float start_y = getFloatInput("start_y");
+    const float end_x = getFloatInput("end_x");
+    const float end_y = getFloatInput("end_y");
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
